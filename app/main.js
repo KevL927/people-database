4c8db88188cf245288eb7105c97080570080d772
KevL927 <leekevin@gmail.com>
2017-03-18 22:51:59 -0400

    Update id name on both files and add script to load on index.hjs

--- app/main.js
+++ app/main.js
@@ -2,15 +2,15 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router';
-import configureStore from './store/configureStore';
-import getRoutes from './routes';
+// import { Router, browserHistory } from 'react-router';
+// import configureStore from './store/configureStore';
+// import getRoutes from './routes';
 
 const store = configureStore(window.INITIAL_STATE);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={getRoutes(store)}/>
+    // <Router history={browserHistory} routes={getRoutes(store)}/>
   </Provider>,
-  document.getElementById('app')
+  document.getElementById('root')
 );
