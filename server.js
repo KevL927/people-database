36eb57b953fdd90192e79dd25d96018c73a83459
KevL927 <leekevin@gmail.com>
2017-03-19 15:39:20 -0400

    Update index with asterisk to avoid conflict with client route

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var index = require('./routes/index');
 var peopleRouter = require('./routes/peopleRouter');
 
 var app = express();
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/people', peopleRouter);
+app.use('/*', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
