63289e760385c7dc04ae41c6289d8258c5d1bd1e
KevL927 <leekevin@gmail.com>
2017-03-19 02:27:20 -0400

    Add get request to return all the people in the DB

--- routes/peopleRouter.js
+++ routes/peopleRouter.js
@@ -4,9 +4,13 @@ var mongoose = require('mongoose');
 
 var People = require('../models/people');
 
-/* GET home page. */
-peopleRouter.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+peopleRouter.get('/', function(req, res) {
+  People.find({}, function(err, people) {
+    if (err) {
+      return res.status(err);
+    }
+    return res.status(200).json(people);
+  });
 });
 
 module.exports = peopleRouter;
