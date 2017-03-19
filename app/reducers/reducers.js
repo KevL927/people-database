f597da6de2ef79c8d67bc3086977f3e33059c458
KevL927 <leekevin@gmail.com>
2017-03-19 17:45:45 -0400

    Create actions and reducers to handle fetching people database. Install isomorphic fetch

--- app/reducers/reducers.js
+++ app/reducers/reducers.js
@@ -1,34 +1,23 @@
-export default function messages(state = {}, action) {
-  switch (action.type) {
-    case 'LOGIN_FAILURE':
-    case 'SIGNUP_FAILURE':
-    case 'UPDATE_PROFILE_FAILURE':
-    case 'CHANGE_PASSWORD_FAILURE':
-    case 'FORGOT_PASSWORD_FAILURE':
-    case 'RESET_PASSWORD_FAILURE':
-    case 'CONTACT_FORM_FAILURE':
-    case 'OAUTH_FAILURE':
-    case 'UNLINK_FAILURE':
-    case 'LINK_FAILURE':
-      return {
-        error: action.messages
-      };
-    case 'UPDATE_PROFILE_SUCCESS':
-    case 'CHANGE_PASSWORD_SUCCESS':
-    case 'RESET_PASSWORD_SUCCESS':
-    case 'CONTACT_FORM_SUCCESS':
-      return {
-        success: action.messages
-      };
-    case 'FORGOT_PASSWORD_SUCCESS':
-    case 'DELETE_ACCOUNT_SUCCESS':
-    case 'UNLINK_SUCCESS':
-      return {
-        info: action.messages
-      };
-    case 'CLEAR_MESSAGES':
-      return {};
-    default:
-      return state;
-  }
+const initialState = {
+	people: null,
+	error: null
+}
+
+export default (state, action) => {
+   state = state || initialState;
+
+    switch(action.type) {
+        case 'FETCH_PEOPLEDATABASE_SUCCESS':
+          return Object.assign({}, state, {
+            people: action.payload
+          });
+
+        case 'FETCH_PEOPLEDATABASE_ERROR':
+          return Object.assign({}, state, {
+              error: action.payload
+          });
+
+        default:
+          return state;
+    }
 }
