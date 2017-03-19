d1eabe111bce9c1e13c200460a617a1e87f5b9cf
KevL927 <leekevin@gmail.com>
2017-03-19 17:42:27 -0400

    Dispatch action to fetch people database when component loads

--- app/components/databaselist.js
+++ app/components/databaselist.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
-export default () => {
-  return (
-    <div className="container">
-      <p>Welcome Aboard~!</p>
-    </div>
-  );
+import * as actions from '../actions/actions';
+
+class DatabaseList extends Component {
+  componentDidMount() {
+    this.props.dispatch(actions.fetchPeopleDatabase());
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <p>Welcome Aboard~!</p>
+      </div>
+    );
+  }
 }
+
+export default connect()(DatabaseList);
