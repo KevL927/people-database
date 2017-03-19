1f25d9e0ef336aa4e1e3b4a83ff221a36f183819
KevL927 <leekevin@gmail.com>
2017-03-19 15:49:57 -0400

    Add dummy components to test

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default () => {
+  return (
+    <div className="container">
+      <h2>People's Database</h2>
+      <h4>Find Out About People</h4>
+      <div>
+        <Link to="/databaselist">Load Database</Link>
+      </div>
+    </div>
+  );
+}
