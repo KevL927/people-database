cca38c5240d0b000a5c02170f35df32d5d11d139
KevL927 <leekevin@gmail.com>
2017-03-20 02:37:34 -0400

    Add componentDidMount for testing purposes

--- app/components/databaselist.js
+++ app/components/databaselist.js
@@ -6,9 +6,11 @@ import * as actions from '../actions/actions';
 class DatabaseList extends Component {
   componentDidMount() {
     this.props.dispatch(actions.fetchPeopleDatabase());
+   this.props.dispatch(actions.putPersonData('58ce201e95362d0d6380f63a', 'hello', 'mr'));
   }
 
   render(){
+    console.log(this.props.people);
     return (
       <div className="container">
         <p>Welcome Aboard~!</p>
@@ -17,4 +19,7 @@ class DatabaseList extends Component {
   }
 }
 
-export default connect()(DatabaseList);
+export default connect(
+  ({ people }) =>
+  ({ people })
+)(DatabaseList);
