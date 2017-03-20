fdbddad065bb07ffafe9e450df74c7b08d36e3bd
KevL927 <leekevin@gmail.com>
2017-03-20 07:56:09 -0400

    Add delete functionality

--- app/components/renderPersonData/PersonData.js
+++ app/components/renderPersonData/PersonData.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default (props) => {
+export default ({personInfo, onClickDeletePersonData }) => {
   return (
     <div className="person-data">
-      <p>{props.personInfo.name} - {props.personInfo.favoriteCity}</p>
+      <span><p>{personInfo.name} - {personInfo.favoriteCity}</p><button onClick={(event) => onClickDeletePersonData(event, personInfo._id)}>Delete</button></span>
     </div>
   );
 }
