da40e2e9d95d4ced2b16c004670e98c80e184b93
KevL927 <leekevin@gmail.com>
2017-03-21 19:12:19 -0400

    Remove DatabaseList component from React routes

--- app/routes.js
+++ app/routes.js
@@ -4,14 +4,12 @@ import { browserHistory, IndexRoute, Route, Router } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import StepOne from './components/StepOne';
-import DatabaseList from './components/DatabaseList';
 
 export default (
     <Router history={browserHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={Home} />
           <Route path="/stepone" component={StepOne} />
-          <Route path="/databaselist" component={DatabaseList} />
         </Route>
     </Router>
 );
