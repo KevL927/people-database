a4c7898a16088a2ef31e7063256b8d5b727bb822
KevL927 <leekevin@gmail.com>
2017-03-22 06:30:02 -0400

    Add StepSix component

--- app/components/steps/StepSix.js
+++ app/components/steps/StepSix.js
@@ -5,27 +5,27 @@ import { Link } from 'react-router';
 import * as actions from '../../actions/actions';
 import RenderStep from '../RenderStep';
 
-class StepFive extends Component {
+class StepSix extends Component {
   componentDidMount() {
     this.props.dispatch(actions.resetPeopleMessageErrorServerStatusResponse());
-    this.props.dispatch(actions.fetchSpecificPersonData(this.props.postPutReturnedObjectId));
+    this.props.dispatch(actions.deletePersonData(this.props.postPutReturnedObjectId));
   }
 
   render() {
     return (
       <div>
         <RenderStep
-          taskTitle={'GET request to \'/people/:personId\' (' + this.props.postPutReturnedObjectId + ') endpoint'}
+          taskTitle={'DELETE request to \'/people/:personId\' (' + this.props.postPutReturnedObjectId + ') endpoint'}
           peopleObject={this.props.people}
+          messageFromServer={this.props.message}
           serverStatusResponse={this.props.serverStatusResponse}
         />
-        <Link to="/stepsix">Next ></Link>
       </div>
     )
   }
 }
 
 export default connect(
-  ({ people, serverStatusResponse, postPutReturnedObjectId }) =>
-  ({ people, serverStatusResponse, postPutReturnedObjectId })
-)(StepFive);
+  ({ people, message, serverStatusResponse, postPutReturnedObjectId }) =>
+  ({ people, message, serverStatusResponse, postPutReturnedObjectId })
+)(StepSix);
