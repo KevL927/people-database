a0f0b2c6039347054ab455107d54a18e8bb21f95
KevL927 <leekevin@gmail.com>
2017-03-21 22:56:40 -0400

    Add StepTwo component to routes

--- app/routes.js
+++ app/routes.js
@@ -3,13 +3,15 @@ import { browserHistory, IndexRoute, Route, Router } from 'react-router';
 
 import App from './components/App';
 import Home from './components/Home';
-import StepOne from './components/StepOne';
+import StepOne from './components/steps/StepOne';
+import StepTwo from './components/steps/StepTwo';
 
 export default (
     <Router history={browserHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={Home} />
           <Route path="/stepone" component={StepOne} />
+          <Route path="/steptwo" component={StepTwo} />
         </Route>
     </Router>
 );
