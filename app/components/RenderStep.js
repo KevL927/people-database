b056bc41dc8f95db34c867d383829c97e2770c74
KevL927 <leekevin@gmail.com>
2017-03-21 20:20:55 -0400

    Remove receiving props from functions

--- app/components/RenderStep.js
+++ app/components/RenderStep.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class RenderStep extends Component {
-  renderServerStatusResponse(props) {
+  renderServerStatusResponse() {
     if (this.props.serverStatusResponse) {
       return (
         <div>
@@ -14,7 +14,7 @@ export default class RenderStep extends Component {
     }
   }
 
-  renderPeopleJson(props) {
+  renderPeopleJson() {
     if (this.props.peopleObject) {
       return (
         <div>
